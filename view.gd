c4933525c30b3fa9f70b0172691112493ed1a307
pyrotek45 <pyrotek45_gaming@yahoo.com>
2023-01-02 04:31:54 -0500

    added zoom and drag options in project input

--- view.gd
+++ view.gd
@@ -1,30 +1,18 @@
 extends Node2D
 onready var camera = $Camera2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var mouse_start_pos
 var screen_start_position
 var dragging = false
 
-var tracking_agent = false
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	#if tracking_agent == false:
-		#$Camera2D.position = get_global_mouse_position()
-	#else:
-		#$Camera2D.position = 
-	
-func _input(event):
 
-	if event.is_action("zoom_up"):
+func _input(event):
+	if event.is_action("zoom_in"):
 		if $Camera2D.zoom > Vector2(.3,.3):
 			$Camera2D.zoom = $Camera2D.zoom - Vector2(0.1,0.1)
 	elif event.is_action("zoom_out"):
@@ -40,9 +28,5 @@ func _input(event):
 	elif event is InputEventMouseMotion and dragging:
 		position = (mouse_start_pos - event.position) + screen_start_position
 
-
-
 func handle_change_view():
 	camera.make_current()
-	
-
