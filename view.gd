86a41fc4ec5e1735ac544adecfde7c65b04db49f
pyrotek45 <pyrotek45_gaming@yahoo.com>
2023-01-02 04:09:33 -0500

    removed side panning in favor of middle click + drag

--- view.gd
+++ view.gd
@@ -4,6 +4,9 @@ onready var camera = $Camera2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var mouse_start_pos
+var screen_start_position
+var dragging = false
 
 var tracking_agent = false
 # Called when the node enters the scene tree for the first time.
@@ -13,20 +16,30 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if tracking_agent == false:
-		$Camera2D.position = get_global_mouse_position()
+	pass
+	#if tracking_agent == false:
+		#$Camera2D.position = get_global_mouse_position()
 	#else:
 		#$Camera2D.position = 
 	
 func _input(event):
-	if event is InputEventMouseButton:
+
+	if event.is_action("zoom_up"):
+		if $Camera2D.zoom > Vector2(.3,.3):
+			$Camera2D.zoom = $Camera2D.zoom - Vector2(0.1,0.1)
+	elif event.is_action("zoom_out"):
+		if $Camera2D.zoom < Vector2(2.5,2.5):
+			$Camera2D.zoom = $Camera2D.zoom + Vector2(0.1,0.1)
+	elif event.is_action("drag"):
 		if event.is_pressed():
-			if event.button_index == BUTTON_WHEEL_UP:
-				if $Camera2D.zoom > Vector2(.3,.3):
-					$Camera2D.zoom = $Camera2D.zoom - Vector2(0.1,0.1)
-			if event.button_index == BUTTON_WHEEL_DOWN:
-				if $Camera2D.zoom < Vector2(2.5,2.5):
-					$Camera2D.zoom = $Camera2D.zoom + Vector2(0.1,0.1)
+			mouse_start_pos = event.position
+			screen_start_position = position
+			dragging = true
+		else:
+			dragging = false
+	elif event is InputEventMouseMotion and dragging:
+		position = (mouse_start_pos - event.position) + screen_start_position
+
 
 
 func handle_change_view():
