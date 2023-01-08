fc7c42ed4e54bdfd6e03e9f02db2565a1eb0b52a
Vincent Ragusa <vincent.ragusa.94@gmail.com>
2023-01-08 04:06:15 -0500

    idk probably nothing

--- Food.gd
+++ Food.gd
@@ -5,8 +5,8 @@ extends KinematicBody2D
 var food_level:int = 5
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+#func _ready():
+#	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
